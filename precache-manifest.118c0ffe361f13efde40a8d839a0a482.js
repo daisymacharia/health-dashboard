57dea64d440522c77771f6e302f3db46dd863579
daisymacharia <daisy.macharia@andela.com>
2020-07-01 19:21:22 +0300

    Updates

---
+++ precache-manifest.118c0ffe361f13efde40a8d839a0a482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a45748cd824d38f94f2385edf756a8",
+    "url": "/health-dashboard/index.html"
+  },
+  {
+    "revision": "3c7089933fc369270ff5",
+    "url": "/health-dashboard/static/css/main.ae374ef2.chunk.css"
+  },
+  {
+    "revision": "cb88476c8941093ea1ad",
+    "url": "/health-dashboard/static/js/2.1b8528c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/health-dashboard/static/js/2.1b8528c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7089933fc369270ff5",
+    "url": "/health-dashboard/static/js/main.72262758.chunk.js"
+  },
+  {
+    "revision": "dc25efb5b98199cb8151",
+    "url": "/health-dashboard/static/js/runtime-main.18122695.js"
+  }
+]);
